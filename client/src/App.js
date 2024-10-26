1f460f6a5169d050119ca15c13d5012f90cb25b0
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-26 10:29:23 +0530

    rt

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { io } from 'socket.io-client';
 import axios from 'axios';
 import Chat from './components/Chat';
+import Login from './pages/Login';
+import { AuthContext } from './context/AuthContext';
 
-const socket = io('http://localhost:5000'); // Server URL
+const socket = io('http://localhost:5000'); // Replace with your server URL
 
 function App() {
+  const { user } = useContext(AuthContext); // Get the user from AuthContext
   const [messages, setMessages] = useState([]);
-  
+
   useEffect(() => {
     const fetchMessages = async () => {
-      const response = await axios.get('http://localhost:5000/api/messages');
-      setMessages(response.data);
+      if (user) {
+        const response = await axios.get('http://localhost:5000/api/messages', {
+          headers: { Authorization: `Bearer ${user.token}` }
+        });
+        setMessages(response.data);
+      }
     };
     fetchMessages();
 
-    socket.on('receiveMessage', (message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
+    if (user) {
+      socket.on('receiveMessage', (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
+      });
+    }
 
     return () => {
       socket.off('receiveMessage');
     };
-  }, []);
+  }, [user]);
 
   return (
     <div className="App">
-      <Chat messages={messages} socket={socket} />
-    </div>
-  );
-}
-
-export default App;
-import { useContext } from 'react';
-import { AuthContext } from './context/AuthContext';
-import Chat from './components/Chat';
-import Login from './pages/Login';
-
-function App() {
-  const { user } = useContext(AuthContext);
-
-  return (
-    <div className="App">
-      {user ? <Chat /> : <Login />}
+      {user ? (
+        <Chat messages={messages} socket={socket} />
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
